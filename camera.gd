d8272a3bc53d816a52fa138bfe0123dd3b474f05
yl74053 <q874982346@gmail.com>
2020-11-10 21:24:30 -0500

    Enable scrolling
    I enable the scrolling to the game with left and right key

---
+++ camera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+var velocity = 10;
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_key_pressed(KEY_LEFT):
+		global_position.x = global_position.x - velocity
+	if Input.is_key_pressed(KEY_RIGHT):
+		global_position.x = global_position.x + velocity
+	pass
